358cf4d03418618434acc06a7428e61bad46f90f
mikegee81 <mikegee81@users.noreply.github.com>
2022-11-30 21:10:02 -0500

    add another test

--- restapi/tests.py
+++ restapi/tests.py
@@ -8,4 +8,7 @@ def two_integers_sum(a, b):
 
 class TestSum(TestCase):
     def test_sum(self):
-        self.assertEqual(two_integers_sum(1, 3), 3)
+        self.assertEqual(two_integers_sum(1, 2), 3)
+    
+    def test_sum_fail(self):
+        self.assertEqual(two_integers_sum(1,2) 5)
